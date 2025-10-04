5e89554b95f2676d1bb0ed042edaeb7b87aca7cf
Ali20165 <alirezaghn25@gmail.com>
2025-10-04 23:59:28 +0330

    Add files via upload

---
+++ Circle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double p = 3.14;
+            double c, mohit, msahat;
+            Console.Write("enter a number: ");
+            c = double.Parse(Console.ReadLine());
+            mohit = c * p;
+            msahat = c * 0.5 * c * 0.5 * p;
+
+            Console.WriteLine("msahat is {0} and mohit is {1} ", msahat, mohit);
+
+            Console.ReadKey();
+
+        }
+    }
+}
