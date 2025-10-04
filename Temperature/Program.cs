407492774df837d4cffb05e6b439662ae2d49129
Ali20165 <alirezaghn25@gmail.com>
2025-10-05 00:00:19 +0330

    Add files via upload

---
+++ Temperature/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, c;
+
+            Console.Write("enter a temp: ");
+            c = Double.Parse(Console.ReadLine());
+            f = c * 1.8 + 32;
+            
+            Console.WriteLine("Fahrenheit = {0} ", f );
+
+            Console.ReadKey();
+        }
+    }
+}
